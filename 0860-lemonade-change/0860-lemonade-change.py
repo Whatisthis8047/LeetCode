ba064bb2ff99e7331d51d056a0b145e5ffd1c48e
Keonmo Lee <rjsah80471@gmail.com>
2024-08-15 11:11:23 +0900

    Time: 769 ms (5.06%), Space: 20 MB (83.97%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        change = {5:0, 10:0, 20:0}
+        for cash in bills:
+            if cash == 5:
+                change[5] += 1
+            elif cash == 10:
+                change[10] += 1
+                if change[5] < 1:
+                    return False
+                change[5] -= 1
+            elif cash == 20:
+                change[20] += 1
+                if change[10] < 1 or change[5] < 1:
+                    if change[5] >= 3:
+                        change[5] -= 3
+                        continue
+                    return False
+                change[5] -= 1
+                change[10] -= 1
+            print(change)
+        return True
