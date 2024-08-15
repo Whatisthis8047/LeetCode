f31f79e1e833a48d941d3157d1b9f52f11c6d2c4
Keonmo Lee <rjsah80471@gmail.com>
2024-08-15 11:24:49 +0900

    Time: 615 ms (69.23%), Space: 21.2 MB (15.58%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,22 +1,25 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         d5, d10 = 0, 0
+
         for cash in bills:
             if cash == 5:
                 d5 += 1
+
             elif cash == 10:
-                if d5 < 1:
+                if d5 >= 1:
+                    d5 -= 1
+                    d10 += 1
+                else:
                     return False
-                d5 -= 1
-                d10 += 1
-            elif cash == 20:
+
+            else:
                 if d10 >= 1 and d5 >= 1:
                     d5 -= 1
                     d10 -= 1
-                    continue
                 elif d5 >= 3:
                     d5 -= 3
-                    continue
-                return False
+                else:
+                    return False
+
         return True
-                
