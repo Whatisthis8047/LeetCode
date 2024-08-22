5cc270ef2254aaea8f300a1b0f78d9bfcd3aa3fa
Keonmo Lee <rjsah80471@gmail.com>
2024-08-22 11:07:56 +0900

    Time: 36 ms (45.66%), Space: 16.5 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,10 +1,9 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        bin_num = bin(num)[2:]
-        res = ""
-        for bit in bin_num:
+        res = ''
+        for bit in bin(num)[2:]:
             if bit == '0':
-                res += '1'
+                res += '1' 
             else:
                 res += '0'
-        return int(res, 2)
+        return int(res,2)
