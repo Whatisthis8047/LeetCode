6089f89eb1dca5209c6f6b6621842772749078a8
Keonmo Lee <rjsah80471@gmail.com>
2024-09-05 09:36:32 +0900

    Time: 989 ms (58.82%), Space: 26.9 MB (52.67%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.py
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -3,13 +3,19 @@ class Solution:
         # brute search
         total = mean * (len(rolls) + n)
         n_rolls = total - sum(rolls)
-        res = []
 
-        if (n_rolls / n > 6) or (n_rolls / n < 1) : return res
+        if n_rolls > 6*n or n_rolls < n : return []
 
-        for i in range(n):
-            x = n_rolls // (n - i)
-            res.append(x)
-            n_rolls -= x
-        
+        avg = n_rolls//n 
+        res = [avg] * n
+        remain = n_rolls - (avg*n)
+        if remain > 0:
+            over = 6 - avg
+            add = remain//over
+            res = res[add:] + [6]*add
+            final = remain%over
+            res[0] += final
         return res
+
+
+        
