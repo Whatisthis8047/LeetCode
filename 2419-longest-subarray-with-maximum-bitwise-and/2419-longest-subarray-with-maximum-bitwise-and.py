a604ac9b7a606c11fec96060fdf31b1edbe4f42c
Keonmo Lee <rjsah80471@gmail.com>
2024-09-14 11:00:20 +0900

    Time: 663 ms (45.46%), Space: 30.8 MB (85.09%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -1,6 +1,5 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        # solving with sliding window technique
         max_num = max(nums)
         res, temp = 0, 0
         for i in range(nums.index(max_num), len(nums)):
