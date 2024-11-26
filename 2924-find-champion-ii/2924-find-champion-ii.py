fee4699221ad362a543a928c7b130986b47b2201
Keonmo Lee <rjsah80471@gmail.com>
2024-11-26 14:06:42 +0900

    Time: 7 ms (68.21%), Space: 17.6 MB (94.59%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -2,12 +2,8 @@ class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         st_node = [0] * n
         for u, v in edges:
-            st_node[v] += 1
+            st_node[v] = 1
         
-        res = None
-        for i in range(len(st_node)):
-            if res == None and st_node[i] == 0:
-                res = i
-            elif st_node[i] == 0:
-                return -1
-        return res
+        if st_node.count(0) == 1:
+            return st_node.index(0)
+        return -1
