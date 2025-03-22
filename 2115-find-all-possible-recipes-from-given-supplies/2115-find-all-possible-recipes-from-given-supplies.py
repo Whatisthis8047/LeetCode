83871acc865a06f2151a2d15886644fcaa140e53
Keonmo Lee <rjsah80471@gmail.com>
2025-03-22 09:23:22 +0900

    Time: 871 ms (5.03%), Space: 19.6 MB (94.65%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,6 +1,5 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        supplies = set(supplies)  # 빠른 조회를 위해 set 사용
         reci_dict = {recipes[i]: ingredients[i] for i in range(len(recipes))}
         result = []
 
@@ -10,7 +9,7 @@ class Solution:
             for recipe, ingredient_list in list(reci_dict.items()):
                 if set(ingredient_list) <= set(supplies):
                     result.append(recipe)
-                    supplies.add(recipe) 
+                    supplies.append(recipe) 
                     del reci_dict[recipe]  
                     found_recipe = True  
             
